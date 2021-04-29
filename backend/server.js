6c067169f4ca3f5c78d99caeeed64d9e45af5ef1
Donghao Wu <donghao.wu.us@gmail.com>
2021-04-29 16:28:09 -0400

    added deploy script and backend code.

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+}
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
